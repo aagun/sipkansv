8d61f76b5813f73ca5825edb0f6dc1d791d942b9
= <agun@duck.com>
2024-06-16 11:45:45 +0700

    Test: controller to create department

---
+++ tests/Feature/DepartmentControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use App\Services\DepartmentService;
+use Illuminate\Http\Response;
+use App\Models\Department;
+use Database\Seeders\DepartmentSeeder;
+
+class DepartmentControllerTest extends TestCase
+{
+    private const BASE_ENDPOINT = '/departments';
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        DB::delete('delete from departments');
+
+        $this->app->make(DepartmentService::class);
+    }
+
+    public function testCreateSuccess()
+    {
+        $department_name = 'DEPARTMENT_NAME_TEST';
+        $payload = [
+            'name' => $department_name,
+            'description' => $department_name
+        ];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertDatabaseHas(Department::class, ['name' => $department_name]);
+    }
+
+    public function testCreateUniqueError()
+    {
+        $this->seed(DepartmentSeeder::class);
+
+        $payload = [
+            'name' => 'UPTD PSDKPWS',
+            'description' => 'Unit Pelaksana Teknis Daerah Penggawasan Sumber Daya Kelautan dan Perikanan Wilayah Selatan',
+        ];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['name' => 'The name has already been taken.']);
+    }
+
+    public function testCreateMandatoryError()
+    {
+        $payload = [];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['name' => 'The name field is required.']);
+    }
+
+
+}
