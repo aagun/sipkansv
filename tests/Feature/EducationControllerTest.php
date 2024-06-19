98a02fa06021c0975af045a52664107e40ff72c5
= <agun@duck.com>
2024-06-19 10:19:03 +0700

    Test: education crud

---
+++ tests/Feature/EducationControllerTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Database\Seeders\DatabaseSeeder;
+use App\Services\EducationService;
+use App\Models\Education;
+
+class EducationControllerTest extends TestCase
+{
+    private const BASE_ENDPOINT = '/educations';
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app->make(EducationService::class);
+    }
+
+    public function testCreateSuccess()
+    {
+        $name = 'EDUCATION_NAME_TEST';
+        $payload = [
+            'name' => $name,
+            'description' => $name
+        ];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertJsonFragment(['message' => __('messages.success.created')]);
+        $this->assertDatabaseHas(Education::class, ['name' => $name]);
+    }
+
+    public function testCreateUniqueError()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $payload = [
+            'name' => 'SMA',
+            'description' => 'Sekolah Menegah Atas',
+        ];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['name' => 'The name has already been taken.']);
+    }
+
+    public function testCreateMandatoryError()
+    {
+        $payload = [];
+
+        $response = $this->post(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['name' => 'The name field is required.']);
+    }
+
+    public function testSearch()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $filter = [
+            'name' => 'D',
+            'description' => 'V'
+        ];
+
+        $response = $this->post( self::BASE_ENDPOINT . "/search", $filter);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment(['message' => __('messages.success.retrieve')]);
+        $response->assertJson(fn (AssertableJson $json) => $json
+            ->count('data', 2)
+            ->etc()
+        );
+    }
+
+    public function testUpdateSuccess()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $id = Education::query()->first()->id;
+        $payload = [
+            'id' => $id,
+            'name' => 'UPDATED_NAME'
+        ];
+
+        $response = $this->put(self::BASE_ENDPOINT, $payload);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment(['message' => __('messages.success.updated')]);
+        $this->assertDatabaseHas(Education::class, ['name' => 'UPDATED_NAME']);
+    }
+
+    public function testUpdateNotExistError()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $id = 1000;
+        $payload = [
+            'id' => $id,
+            'name' => 'UPDATE_NAME'
+        ];
+
+        $response = $this->put(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['id' => 'The selected id is invalid.']);
+    }
+
+    public function testUpdateUniqueError()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $current_data = Education::query()->first();
+
+        $payload = [
+            'id' => $current_data->id,
+            'name' => $current_data->name
+        ];
+
+        $response = $this->put(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['name' => 'The name has already been taken.']);
+    }
+
+    public function testUpdateMandatoryError()
+    {
+        $payload = [];
+
+        $response = $this->put(self::BASE_ENDPOINT, $payload);
+
+        $response->assertStatus(Response::HTTP_BAD_REQUEST);
+        $response->assertInvalid(['id' => 'The id field is required.']);
+    }
+
+    public function testDeleteSuccess()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $id = Education::query()->first()->id;
+
+        $response = $this->delete(self::BASE_ENDPOINT . '/' . $id);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment(['message' => __('messages.success.deleted')]);
+        $this->assertDatabaseCount(Education::class, 5);
+    }
+
+    public function testDeleteFailed()
+    {
+        $id = 10;
+        $response = $this->delete(self::BASE_ENDPOINT .  '/' . $id);
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+        $response->assertInvalid(['id' => "The selected $id is invalid."]);
+    }
+
+}
