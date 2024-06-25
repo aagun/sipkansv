d864c9bffd4f4bf94a2f0e4c2c65fbd8ceb9a7d9
= <agun@duck.com>
2024-06-25 13:33:18 +0700

    Refactor: routes

--- routes/web.php
+++ routes/web.php
@@ -25,61 +25,33 @@ use App\Http\Controllers\VillageController;
 use App\Http\Controllers\ActivityReportController;
 use App\Http\Controllers\ActivityController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect('/login'));
 
 Route::prefix("/dashboard")
     ->name("dashboard.")
     ->group(function () {
-        Route::get("/", function() {
-            return view("components.dashboard.dashboard-layout");
-        })->name("index");
+        Route::prefix('/users')
+            ->group(function () {
+                Route::get("/", fn () => view("components.dashboard.dashboard-layout"))->name("index");
+                Route::get("/users", fn () =>view("components.dashboard.users.dashboard-users"))->name("users");
+                Route::get("/users/create", fn () => view("components.dashboard.users.form-users"))->name("users");
+            });
 
-        Route::get("/users", function() {
-            return view("components.dashboard.users.dashboard-users");
-        })->name("users");
-        Route::get("/users/create", function() {
-            return view("components.dashboard.users.form-users");
-        })->name("users");
-        Route::post("/users/create", function() {
-            $request = request()->all();
-
-            if ($request["password"] != $request["confirm-password"]) {
-                return redirect()->back()->with("error", "Password tidak sama");
-            }
-        })->name("users");
-
-        Route::prefix("/data")
-        ->name("data.")
-        ->group(function() {
-            Route::get("/pangkat", function() {
-                return view("components.dashboard.masterdata.rank");
-            })->name("ranks");
-            Route::get("/jabatan", function() {
-                return view("components.dashboard.masterdata.position");
-            })->name("position");
-            Route::get("/golongan-ruang", function() {
-                return view("components.dashboard.masterdata.comity");
-            })->name("comity");
-            Route::get("/pendidikan", function() {
-                return view("components.dashboard.masterdata.education");
-            })->name("education");
-            Route::get("/unit-kerja", function() {
-                return view("components.dashboard.masterdata.department");
-            })->name("department");
-            Route::get("/instansi", function() {
-                return view("components.dashboard.masterdata.institution");
-            })->name("institution");
-        });
+        Route::prefix("/data")->name("data.")
+            ->group(function() {
+                Route::get("/pangkat", fn () => view("components.dashboard.masterdata.rank"))->name("ranks");
+                Route::get("/jabatan", fn () => view("components.dashboard.masterdata.position"))->name("position");
+                Route::get("/golongan-ruang", fn () => view("components.dashboard.masterdata.comity"))->name("comity");
+                Route::get("/pendidikan", fn () => view("components.dashboard.masterdata.education"))->name("education");
+                Route::get("/unit-kerja", fn () => view("components.dashboard.masterdata.department"))->name("department");
+                Route::get("/instansi", fn () => view("components.dashboard.masterdata.institution"))->name("institution");
+            });
 
     });
 
 Route::name("auth.")
     ->group(function () {
-        Route::get("/login", function() {
-            return view("login");
-        })->name("login");
+        Route::get("/login", fn () => view("login"))->name("login");
         Route::post("/login", function() {
             $data = ["username" => "admin", "password" => "123456"];
             $admin = Arr::first($data, fn($d) => $d == request()->input("username"));
