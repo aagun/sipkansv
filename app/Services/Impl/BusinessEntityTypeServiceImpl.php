a18e8a813f6c46bca3aad3e2da7df375d2e7c912
= <agun@duck.com>
2024-06-28 15:28:50 +0700

    Modify: business entity types pagination search service

--- app/Services/Impl/BusinessEntityTypeServiceImpl.php
+++ app/Services/Impl/BusinessEntityTypeServiceImpl.php
@@ -49,6 +49,13 @@ class BusinessEntityTypeServiceImpl implements BusinessEntityTypeService
 
     public function search(array $filter): LengthAwarePaginator
     {
+        if ($filter['limit'] == 0) $filter['limit'] = $this->searchMainQuery($filter)->count();
+        return $this->searchMainQuery($filter)
+            ->paginate(perPage: $filter['limit'], page: $filter['offset']);
+    }
+
+    private function searchMainQuery(array $filter)
+    {
         $search = $filter['search'];
         $order = $filter['order'];
         $permissibleSort = ['name', 'description'];
@@ -64,10 +71,8 @@ class BusinessEntityTypeServiceImpl implements BusinessEntityTypeService
                     $query->whereRaw("description LIKE CONCAT('%', ?, '%')", [$search['description']]);
                 }
             })
-            ->orderByRaw("$sort $order")
-            ->paginate(perPage: $filter['limit'], page: $filter['offset']);
+            ->orderByRaw("$sort $order");
     }
-
     public function save(array $businessEntityType): Model | Builder
     {
         return BusinessEntityType::query()->create($businessEntityType);
