08426b3af06c234a67142aaec7a7897cf39f366c
Eri <eridwiatmoko@gmail.com>
2024-06-29 20:13:59 +0700

    Update: code

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::group([
     'prefix' => 'auth',
     'middleware' => 'api'
 ], function () {
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
     Route::middleware('auth:api')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
